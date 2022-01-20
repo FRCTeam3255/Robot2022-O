3b0dc75cf545e9e83d8be549580fb10c11e99007
LookItsAndy <59989200+LookItsAndy@users.noreply.github.com>
2022-01-19 17:14:54 -0800

    Created Climber subsystem
    - TalonFX
    - Mag swtich

---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
+
+public class Climber extends SubsystemBase {
+
+  /** Creates a new Climber. */
+  private TalonFX climbMotor;
+  private DigitalInput safetyMagSwitch;
+
+  public Climber() {
+
+    safetyMagSwitch = new DigitalInput(RobotMap.Climber.SAFETY_MAG_SWITCH);
+    climbMotor = new TalonFX(RobotMap.Climber.CLIMBER_MOTOR);
+    configure();
+
+  }
+
+  private void configure() {
+    climbMotor.configFactoryDefault();
+
+  }
+
+  public double getClimberEncoderCount() {
+    return climbMotor.getSelectedSensorPosition();
+
+  }
+
+  public boolean isSafetyMagSwitch() {
+    return safetyMagSwitch.get();
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
