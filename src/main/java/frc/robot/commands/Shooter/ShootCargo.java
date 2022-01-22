5a359115bd96bd680714e552583b858badb52b45
Alice <90939494+ACat701@users.noreply.github.com>
2022-01-22 14:52:11 -0800

    Added Initialization

--- src/main/java/frc/robot/commands/Shooter/ShootCargo.java
+++ src/main/java/frc/robot/commands/Shooter/ShootCargo.java
@@ -22,6 +22,7 @@ public class ShootCargo extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    shooter.setShooterSpeed(RobotPreferences.ShooterPrefs.shooterMotorSpeed.getValue());
   }
 
   // Called every time the scheduler runs while the command is scheduled.
