75d6305bd12d647c6b0bac9726f62f95f87434ea
Alice <90939494+ACat701@users.noreply.github.com>
2022-01-22 10:41:59 -0800

    Finished Creating Intake Subsystem

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -8,6 +8,8 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
@@ -20,14 +22,34 @@ public class Intake extends SubsystemBase {
   // Link to Robot Map
   public Intake() {
     IntakeMotor = new TalonFX(RobotMap.Intake.INTAKE_MOTOR_CAN);
+    IntakeSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, RobotMap.Intake.INTAKE_SOLENOID_CAN_A,
+        RobotMap.Intake.INTAKE_SOLENOID_CAN_B);
 
-    // why no work?????????????
-    // IntakeSolenoid = new DoubleSolenoid(RobotMap.Intake.INTAKE_SOLENOID_CAN_A,
-    // RobotMap.Intake.INTAKE_SOLENOID_CAN_B);
+    configure();
+  }
+
+  // Set to factory default (configure it)
+  public void configure() {
+    IntakeMotor.configFactoryDefault();
+  }
+
+  // Reset Encoder Count
+  public void resetEncoderCount() {
+    IntakeMotor.setSelectedSensorPosition(0);
+  }
+
+  // Get positons
+  public double getIntakeMotorCount() {
+    return IntakeMotor.getSelectedSensorPosition();
+  }
+
+  public Value getSolenoidPosition() {
+    return IntakeSolenoid.get();
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Intake Motor", getIntakeMotorCount());
   }
 }
