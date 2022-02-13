FRCTeam3255/Robot2022-O

commit 292b6d9edfb03d33aead5752b134246bcfdc8f68

updated shooter subsystem (#116)

src/main/java/frc/robot/subsystems/Shooter.java
@@ -5,67 +5,80 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
+import frc.robot.RobotPreferences;
 
 public class Shooter extends SubsystemBase {
 
   // CREATES NEW MOTOR
-  private TalonFX leftMotor;
-  private TalonFX rightMotor;
+  private TalonFX leadMotor;
+  private TalonFX followMotor;
+
+  private TalonFXConfiguration config;
 
   // LINKS TO ROBOT MAP
   public Shooter() {
-    leftMotor = new TalonFX(RobotMap.ShooterMap.LEFT_MOTOR_CAN);
-    rightMotor = new TalonFX(RobotMap.ShooterMap.RIGHT_MOTOR_CAN);
+    leadMotor = new TalonFX(RobotMap.ShooterMap.LEFT_MOTOR_CAN);
+    followMotor = new TalonFX(RobotMap.ShooterMap.RIGHT_MOTOR_CAN);
 
     configure();
   }
 
   // Configures Shooter Motor's factory Defaults
   public void configure() {
-    leftMotor.configFactoryDefault();
-    rightMotor.configFactoryDefault();
+    leadMotor.configFactoryDefault();
+    followMotor.configFactoryDefault();
 
-    leftMotor.setInverted(false);
-    rightMotor.setInverted(true);
+    config.slot0.kF = RobotPreferences.ShooterPrefs.shooterF.getValue();
+    config.slot0.kP = RobotPreferences.ShooterPrefs.shooterP.getValue();
+    config.slot0.kI = RobotPreferences.ShooterPrefs.shooterI.getValue();
+    config.slot0.kD = RobotPreferences.ShooterPrefs.shooterD.getValue();
+
+    leadMotor.configAllSettings(config);
+    followMotor.configAllSettings(config);
+
+    followMotor.follow(leadMotor);
+
+    leadMotor.setInverted(false);
+    followMotor.setInverted(InvertType.OpposeMaster);
+
+    leadMotor.setSensorPhase(false);
+    followMotor.setSensorPhase(true);
   }
 
   // RESETS COUNT FOR ENCODERS
   public void resetShooterEncoderCounts() {
-    leftMotor.setSelectedSensorPosition(0);
-    rightMotor.setSelectedSensorPosition(0);
+    leadMotor.setSelectedSensorPosition(0);
+    followMotor.setSelectedSensorPosition(0);
   }
 
   // Gets and returns shooter's encoder counts
-  public double getLeftMotorEncoderCount() {
-    return leftMotor.getSelectedSensorPosition();
-  }
-
-  public double getRightMotorEncoderCount() {
-    return rightMotor.getSelectedSensorPosition();
+  public double getShooterEncoderCount() {
+    return leadMotor.getSelectedSensorPosition();
   }
 
   // Sets/Controls Shooter Motor speeds
   public void setShooterSpeed(double a_speed) {
     double speed = a_speed;
 
-    leftMotor.set(ControlMode.PercentOutput, speed);
-    rightMotor.set(ControlMode.PercentOutput, speed);
+    leadMotor.set(ControlMode.PercentOutput, speed);
 
   }
 
   public double getShooterVelocity() {
-    return leftMotor.getSelectedSensorVelocity();
+    return leadMotor.getSelectedSensorVelocity();
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    SmartDashboard.putNumber("Shooter Left Motor", getLeftMotorEncoderCount());
-    SmartDashboard.putNumber("Shooter Right Motor", getRightMotorEncoderCount());
+    SmartDashboard.putNumber("Shooter Left Motor", getShooterEncoderCount());
     SmartDashboard.putNumber("Shooter Velocity", getShooterVelocity());
   }
 }

src/main/java/frc/robot/RobotPreferences.java
@@ -14,6 +14,11 @@ public final class RobotPreferences {
     public static final SN_DoublePreference shooterMotorSpeed = new SN_DoublePreference("shooterMotorSpeed", 1);
     public static final SN_DoublePreference shooterMotorTargetVelocity = new SN_DoublePreference(
         "shooterMotorTargetVelocity", 5000.0);
+
+    public static final SN_DoublePreference shooterF = new SN_DoublePreference("kF", 0);
+    public static final SN_DoublePreference shooterP = new SN_DoublePreference("kP", 1);
+    public static final SN_DoublePreference shooterI = new SN_DoublePreference("kI", 0);
+    public static final SN_DoublePreference shooterD = new SN_DoublePreference("kD", 0);
   }
 
   public static final class TurretPrefs {

commit 45ac12cf7a39423595e8e5707e0bf12d3cbaaa7e

fixed null exception error introduced in #116 intializes config variable in Shooter subsystem

src/main/java/frc/robot/subsystems/Shooter.java
@@ -20,7 +20,7 @@ public class Shooter extends SubsystemBase {
   private TalonFX leadMotor;
   private TalonFX followMotor;
 
-  private TalonFXConfiguration config;
+  private TalonFXConfiguration config = new TalonFXConfiguration();
 
   // LINKS TO ROBOT MAP
   public Shooter() {