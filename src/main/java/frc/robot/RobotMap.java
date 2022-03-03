832cf423a7b757903c73a36cabbcdf16ed347851
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2022-03-02 17:15:05 -0800

    update to robotmap and build.gradle (#184)
    * update to robotmap and build.gradle
    
    * Ordered subclasses alphabetically
    
    Closes #185
    
    * Changed port numbers
    
    Changed port numbers to be in order for CAN, PCM, and DIO
    
    * Added missing space
    
    Added divider to separate intake CAN and PCM
    
    Co-authored-by: Errorz <kane.barnes@gmail.com>

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -20,61 +20,63 @@ package frc.robot;
 // THE CAN PORTS FOR EVERY SUBSYSTEM WILL GO UP BY 10
 
 public final class RobotMap {
-    public final class DrivetrainMap {
-        public static final int LEFT_LEAD_MOTOR_CAN = 10;
-        public static final int RIGHT_LEAD_MOTOR_CAN = 11;
-        public static final int LEFT_FOLLOW_MOTOR_CAN = 12;
-        public static final int RIGHT_FOLLOW_MOTOR_CAN = 13;
-    }
 
-    public final class HoodMap {
-        public static final int HOOD_SOLENOID_STEEP_ANGLE_PCM_A = 0;
-        public static final int HOOD_SOLENOID_SHALLOW_ANGLE_PCM_B = 1;
-    }
+  public final class ClimberMap {
+    public static final int CLIMBER_MOTOR_CAN = 10;
 
-    public final class TurretMap {
-        public static final int TURRET_MOTOR_CAN = 20;
-    }
+    public static final int BOTTOM_SAFETY_MAG_SWITCH_DIO = 0;
 
-    public final class TransferMap {
+    public static final int LOCK_PISTON_PCM_A = 0;
+    public static final int LOCK_PISTON_PCM_B = 1;
+    public static final int PIVOT_PISTON_PCM_A = 2;
+    public static final int PIVOT_PISTON_PCM_B = 3;
+  }
 
-        public static final int TOP_BELT_MOTOR_CAN = 30;
-        public static final int BOTTOM_BELT_MOTOR_CAN = 31;
-        public static final int ENTRANCE_BELT_MOTOR_CAN = 32;
-        public static final int TRANSFER_TOP_LEFT_LIMIT_SWITCH_DIO = 0;
-        public static final int TRANSFER_BOTTOM_LEFT_LIMIT_SWITCH_DIO = 1;
-        public static final int TRANSFER_TOP_RIGHT_LIMIT_SWITCH_DIO = 2;
-        public static final int TRANSFER_BOTTOM_RIGHT_LIMIT_SWITCH_DIO = 3;
+  public final class ControllerMap {
+    public static final int DRIVER_STICK = 0;
+    public static final int CODRIVER_STICK = 1;
+  }
 
-    }
+  public final class DrivetrainMap {
+    public static final int LEFT_LEAD_MOTOR_CAN = 20;
+    public static final int LEFT_FOLLOW_MOTOR_CAN = 21;
+    public static final int RIGHT_LEAD_MOTOR_CAN = 22;
+    public static final int RIGHT_FOLLOW_MOTOR_CAN = 23;
+  }
 
-    public final class IntakeMap {
+  public final class HoodMap {
+    public static final int HOOD_SOLENOID_STEEP_ANGLE_PCM_A = 4;
+    public static final int HOOD_SOLENOID_SHALLOW_ANGLE_PCM_B = 5;
+  }
 
-        public static final int INTAKE_MOTOR_CAN = 40;
-        public static final int INTAKE_SOLENOID_PCM_A = 2;
-        public static final int INTAKE_SOLENOID_PCM_B = 3;
-    }
+  public final class IntakeMap {
+    public static final int INTAKE_MOTOR_CAN = 30;
 
-    public final class VisionMap {
-    }
+    public static final int INTAKE_SOLENOID_PCM_A = 6;
+    public static final int INTAKE_SOLENOID_PCM_B = 7;
+  }
 
-    public final class ClimberMap {
-        public static final int CLIMBER_MOTOR_CAN = 50;
-        public static final int BOTTOM_SAFETY_MAG_SWITCH_DIO = 4;
-        public static final int LOCK_PISTON_PCM_A = 4;
-        public static final int LOCK_PISTON_PCM_B = 5;
-        public static final int PIVOT_PISTON_PCM_A = 6;
-        public static final int PIVOT_PISTON_PCM_B = 7;
-    }
+  public final class ShooterMap {
+    public static final int LEFT_MOTOR_CAN = 40;
+    public static final int RIGHT_MOTOR_CAN = 41;
+  }
 
-    public final class ShooterMap {
-        public static final int LEFT_MOTOR_CAN = 60;
-        public static final int RIGHT_MOTOR_CAN = 61;
-    }
+  public final class TransferMap {
+    public static final int ENTRANCE_BELT_MOTOR_CAN = 50;
+    public static final int BOTTOM_BELT_MOTOR_CAN = 51;
+    public static final int TOP_BELT_MOTOR_CAN = 52;
 
-    public final class ControllerMap {
+    public static final int TRANSFER_TOP_LEFT_LIMIT_SWITCH_DIO = 1;
+    public static final int TRANSFER_BOTTOM_LEFT_LIMIT_SWITCH_DIO = 2;
+    public static final int TRANSFER_TOP_RIGHT_LIMIT_SWITCH_DIO = 3;
+    public static final int TRANSFER_BOTTOM_RIGHT_LIMIT_SWITCH_DIO = 4;
+  }
 
-        public static final int DRIVER_STICK = 0;
-        public static final int CODRIVER_STICK = 1;
-    }
+  public final class TurretMap {
+    public static final int TURRET_MOTOR_CAN = 60;
+  }
+
+  public final class VisionMap {
+  }
+
 }
